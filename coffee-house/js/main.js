df8d558f03739db1773b7aa5c6bb3430304ed317
Vsevolod Goriachev <sevak_08@mail.ru>
2023-12-08 20:18:41 +0300

    feat: create slider and add setInterval

--- coffee-house/js/main.js
+++ coffee-house/js/main.js
@@ -2,6 +2,18 @@
 
 const header = document.querySelector('header');
 const menuButton = document.querySelector('.menu-button');
+const sliderSection = document.querySelector('.section-favorite');
+const btns = document.querySelectorAll('.slider-button');
+const sliderWrappers = document.querySelectorAll('.coffee-slider-wrapper');
+let indexImg = 1;
+let sliderPosition = 0;
+let timer = 5000;
+let interval;
+let remain = timer;
+let startTime;
+
+
+
 
 header.addEventListener('click', function(event){
     if (menuButton.contains(event.target)){
@@ -28,3 +40,67 @@ function openBurger(){
     }
     
 }
+
+sliderSection.addEventListener('click', function(event){
+    const nextBtn = document.querySelector('.slider-button-right');
+    const prevBtn = document.querySelector('.slider-button-left');
+    if (nextBtn.contains(event.target)) {
+            showNextSlide();
+    }
+    if (prevBtn.contains(event.target)) {
+            showPrevSlide();
+    }
+})
+
+
+function changeCurrentPosition(){
+    const position = `${-sliderPosition * 100}%`;
+    document.querySelector('.slider-common-wrapper').style.transform = `translateX(${position})`;  
+    clearInterval(interval);
+    startTime = Date.now();
+    remain = timer;
+    interval = setInterval(showNextSlide, remain);
+    document.querySelector('.slider-span-active').classList.remove('slider-span-active');
+    document.querySelector(`.slider-span-${sliderPosition + 1}`).classList.add('slider-span-active');
+
+}
+
+function showNextSlide(){
+    sliderPosition++;
+    if (sliderPosition == 3) {
+        sliderPosition = 0;
+    }
+    changeCurrentPosition();
+}
+
+function showPrevSlide(){
+    if (sliderPosition == 0) {
+        sliderPosition = 3;
+    }
+    sliderPosition--;
+    changeCurrentPosition();
+}
+
+
+function runInterval(){
+    interval = setInterval(showNextSlide, remain)
+    startTime = Date.now();
+}
+
+function pauseInterval(){
+    clearInterval(interval);
+    remain = remain - (Date.now() - startTime);
+}
+
+
+sliderSection.addEventListener('mouseenter', function(){
+    pauseInterval();
+})
+
+sliderSection.addEventListener('mouseleave', function(){
+    runInterval();
+})
+
+
+
+runInterval();
